d7b5c0fd6c39b3d72cc6adf9a4da45210ead5e79
Mirosław Sztorc <mirek.sztorc@gmail.com>
2017-03-20 16:53:21 +0100

    log_errors added

--- config.php
+++ config.php
@@ -6,8 +6,9 @@ return (object) [
     'password' => 'KqvsoFLT2M',
     'wsdl' => 'https://dpdservicesdemo.dpd.com.pl/DPDPackageObjServicesService/DPDPackageObjServices?WSDL',
     'lang_code' => 'PL',
-    'api_version' => 2,
-    'debug' => false,
+    'api_version' => 1,
+    'debug' => true,
+    'log_errors' => false,
     'log_path' => 'logs',
     'timezone' => 'Europe/Warsaw'
 ];
